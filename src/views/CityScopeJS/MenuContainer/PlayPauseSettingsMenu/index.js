b2eb522e0a9c216b5164a4cc4732b5625d302d69
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-11-04 12:47:05 +0100

    PlayPauseSettingsMenu

---
+++ src/views/CityScopeJS/MenuContainer/PlayPauseSettingsMenu/index.js
@@ -0,0 +1,12 @@
+import { List } from "@mui/material";
+import PlayPauseSubmenu from "./PlayPauseSubmenu";
+
+function PlayPauseSettingsMenu() {
+  return (
+    <List>
+      <PlayPauseSubmenu />
+    </List>
+  );
+}
+
+export default PlayPauseSettingsMenu;
